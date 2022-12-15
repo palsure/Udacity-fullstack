05a069cb97a47fbe66bdd03b6bc4bb47b628262b
sureshpalus <106168916+sureshpalus@users.noreply.github.com>
2022-12-14 23:26:28 -0500

    updates

--- 3-coffee-shop/starter_code/frontend/src/environments/environment.ts
+++ 3-coffee-shop/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'sure-coffee.us.auth0.com', // the auth0 domain prefix
-    audience: 'https://sure-coffee-shop.com', // the audience set for the auth0 app
-    clientId: 'aEpyYqLpDOBnMm3CVCg6TjDHtfXtrr01', // the client id generated for the auth0 app
+    url: 'sure-coffee-shop.us.auth0.com', // the auth0 domain prefix
+    audience: 'sure-coffee', // the audience set for the auth0 app
+    clientId: 'EFfOUo6dz330jdzjydFlHEKh6LVfb2ji', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
